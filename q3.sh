2f3a5a044e82dc50c7a762a6d42bdfbb8515797a
Abhijith <abhijith.anil@students.iiit.ac.in>
2021-06-14 08:59:05 +0530

    updated q3

--- q3.sh
+++ q3.sh
@@ -2,14 +2,14 @@
 clear
 #echo input is taken from a text file called q3.txt
 echo "1: Word start with 's' and is not followed by 'a'"
-grep -o  "\bs[^a]\w*" $1
+grep -o  "\bs[^a]\w*" $1 | tr '\n' ', '
 echo
 echo "2: Word start with 'w' and is followed by 'h'"
-grep -o "\bwh\w*" $1
+grep -o "\bwh\w*" $1 | tr '\n' ', '
 echo
 echo "3: Word start with 't' and is followed by 'h'"
-grep -o "\bth\w*" $1
+grep -o "\bth\w*" $1 | tr '\n' ', '
 echo
 echo "4: Word starts with 'a' and is not followed by 'n'"
-grep -o "\ba[^n]\w*" $1
+grep -o "\ba[^n]\w*" $1 | tr '\n' ', '
 echo
